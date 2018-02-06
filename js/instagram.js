72a8aae4391c1ffbe0e66d656c2147478c08de35
Dooho Park <pakddo@gmail.com>
2018-02-06 12:08:47 +0900

    loader

--- js/instagram.js
+++ js/instagram.js
@@ -23,7 +23,7 @@ $(function(){
 						createPhotoElement(res.data[i])
 				  );
 			    }
-				// $('.loader').fadeOut(300);
+				$('.loader').fadeOut(300);
 				$(chunk).fadeIn(500, function() {
 					if ($(chunk).position().top > 100) {
 						$('html,body').animate({ scrollTop: $(chunk).offset().top }, 500, 'easeInOutSine');
